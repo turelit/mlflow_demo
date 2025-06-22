bf237d842ce2814fc3dfad5469f04ebd8ad58d0f
rafal.turel <rafal.turel@gmail.com>
2025-06-22 21:09:23 +0200

    updated_policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,10 +1,26 @@
 resource "aws_iam_role" "sagemaker_role" {
-  name               = "sagemaker_role"
-  path               = "/"
-  assume_role_policy = data.aws_iam_policy_document.sagemaker_policy.json
+  name                  = "sagemaker_role"
+  path                  = "/"
+  assume_role_policy    = data.aws_iam_policy_document.sagemaker_assume_policy_document.json
+  inline_policy {
+    name   = "sagemaker_role_inline_policy"
+    policy = data.aws_iam_policy_document.sagemaker_inline_policy_document.json
+  }
 }
 
-data "aws_iam_policy_document" "sagemaker_policy" {
+data "aws_iam_policy_document" "sagemaker_inline_policy_document" {
+  statement {
+    actions   = [
+        "sagemaker:DescribeDomain",
+        "sagemaker:listSpaces",
+        "sagemaker:listApps",
+        "sagemaker:describeApp",
+    ]
+    resources = ["arn:aws:sagemaker*"]
+  }
+}
+
+data "aws_iam_policy_document" "sagemaker_assume_policy_document" {
   statement {
     actions = ["sts:AssumeRole"]
 
@@ -12,17 +28,5 @@ data "aws_iam_policy_document" "sagemaker_policy" {
       type        = "Service"
       identifiers = ["sagemaker.amazonaws.com"]
     }
-  }
-  statement {
-    actions = [
-      "sagemaker:DescribeDomain",
-      "sagemaker:listSpaces",
-      "sagemaker:listApps",
-      "sagemaker:describeApp",
-    ]
-
-    resources = [
-      "arn:aws:sagemaker:*",
-    ]
   }
 }
