31e1ca30f3a3f07167f7c4e0d398143d8286ceaa
rafal.turel <rafal.turel@gmail.com>
2025-06-22 19:31:56 +0200

    syntax_fixes

--- terraform/sagemaker.tf
+++ terraform/sagemaker.tf
@@ -1,11 +1,11 @@
 resource "aws_sagemaker_domain" "mlflow_demo" {
   domain_name = "mlflow_demo"
   auth_mode   = "IAM"
-  vpc_id      = aws_vpc.default.id
-  subnet_ids  = [aws_subnet.default.id]
+  vpc_id      = data.aws_vpc.default.id
+  subnet_ids  = [data.aws_subnet.default.id]
 
   default_user_settings {
-    execution_role = aws_iam_role.example.arn
+    execution_role = aws_iam_role.sagemaker_role.arn
   }
 }
 
