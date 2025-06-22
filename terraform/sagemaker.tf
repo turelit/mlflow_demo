e616aef513300f1a1050c79f7b2aa5551cc1753a
rafal.turel <rafal.turel@gmail.com>
2025-06-22 19:42:58 +0200

    fixed_domain_name

--- terraform/sagemaker.tf
+++ terraform/sagemaker.tf
@@ -1,5 +1,5 @@
 resource "aws_sagemaker_domain" "mlflow_demo" {
-  domain_name = "mlflow_demo"
+  domain_name = "mlflow-demo"
   auth_mode   = "IAM"
   vpc_id      = data.aws_vpc.default.id
   subnet_ids  = [data.aws_subnet.default_az1.id,data.aws_subnet.default_az2.id,data.aws_subnet.default_az3.id]
