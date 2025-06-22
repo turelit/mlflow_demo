1ee7e9183d841709f3df3e4122d21978570c5128
rafal.turel <rafal.turel@gmail.com>
2025-06-22 19:40:46 +0200

    fixed_default_subnets

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,3 +1,18 @@
 data "aws_vpc" "default" {
   default = true
 } 
+
+data "aws_subnet" "default_az1" {
+  availability_zone = "az1"
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_subnet" "default_az2" {
+  availability_zone = "az2"
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_subnet" "default_az3" {
+  availability_zone = "az3"
+  vpc_id = data.aws_vpc.default.id
+}
--- terraform/sagemaker.tf
+++ terraform/sagemaker.tf
@@ -2,7 +2,7 @@ resource "aws_sagemaker_domain" "mlflow_demo" {
   domain_name = "mlflow_demo"
   auth_mode   = "IAM"
   vpc_id      = data.aws_vpc.default.id
-  subnet_ids  = [data.aws_subnet.default.id]
+  subnet_ids  = [data.aws_subnet.default_az1.id,data.aws_subnet.default_az2.id,data.aws_subnet.default_az3.id]
 
   default_user_settings {
     execution_role = aws_iam_role.sagemaker_role.arn
