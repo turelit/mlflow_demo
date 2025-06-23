db500da36fb0d3aeb5b4c1df51de124c230d82c9
rafal.turel <rafal.turel@gmail.com>
2025-06-23 19:03:54 +0200

    syntax fix

--- terraform/data.tf
+++ terraform/data.tf
@@ -8,7 +8,7 @@ data "aws_subnet" "default_az1" {
 }
 
 data "aws_subnet" "default_az2" {
-  availability_zone = "${var.region}"
+  availability_zone = "${var.region}b"
   vpc_id = data.aws_vpc.default.id
 }
 
