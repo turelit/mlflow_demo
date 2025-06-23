000cc4ea957ad9ef831a737076e81b7b5223ed07
rafal.turel <rafal.turel@gmail.com>
2025-06-23 09:03:18 +0200

    added default space settings for domain

--- terraform/sagemaker.tf
+++ terraform/sagemaker.tf
@@ -7,6 +7,17 @@ resource "aws_sagemaker_domain" "mlflow_demo" {
   default_user_settings {
     execution_role = aws_iam_role.sagemaker_role.arn
   }
+  default_space_settings {
+    execution_role = aws_iam_role.sagemaker_role.arn
+    jupyter_server_app_settings {
+      code_repository {
+        repository_url = aws_sagemaker_code_repository.mlflow_demo_repository.repository_url
+      }
+      default_resource_spec {
+        instance_type = "ml.t3.medium"
+      }
+    }
+  }
 }
 
 resource "aws_sagemaker_user_profile" "turelit" {
